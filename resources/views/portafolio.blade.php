8729ca548ddb522b3b387d954b3850ea5109afd1
DonovanQ02 <166979466+DonovanQ02@users.noreply.github.com>
2024-10-16 16:34:43 -0600

    datos
    Agregamos los datos a la vista

--- resources/views/portafolio.blade.php
+++ resources/views/portafolio.blade.php
@@ -10,18 +10,30 @@
         {{--inicia navbar--}}
     <ul class="nav nav-tabs">
     <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="#">Active</a>
+        <a class="nav-link active" aria-current="page" href="#">Portafolio</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="#">Active</a>
+        <a class="nav-link" aria-current="page" href="#">Repaso 1</a>
     </li>
-    <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="#">Active</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="#">Active</a>
-    </li>
+    
     </ul>
     {{--termina navbar--}}
+    
+
+
+<div class="container mt-4">
+    <div class="card bg-dark text-light">
+        <div class="card-body">
+            <h5 class="card-title">Donovan Ivan López Quintero</h5>
+            <p class="card-text">S-192</p>
+            <p class="card-text">7mo cuatrimestre</p>
+            
+            <p class="card-text">Descubre los repasos de la meteria programación web</p>
+            <p class="card-text">Profesor: Ivan Isay Guerra Lopez</p>
+            
+            <a href="#" class="btn btn-primary">Repaso 1</a>
+        </div>
+    </div>
+</div>
 </body>
 </html>
