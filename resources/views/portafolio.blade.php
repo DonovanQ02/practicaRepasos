ec23ed28dcf6624fcca06e8abbc3967bfc313527
DonovanQ02 <166979466+DonovanQ02@users.noreply.github.com>
2024-10-16 16:25:29 -0600

    navbar
    agregamos una primera navbar

---
+++ resources/views/portafolio.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite(['C:\laragon\www\practicaRepasos\resources\js\app.js'])
+    <title>Portafolio</title>
+</head>
+<body>
+        {{--inicia navbar--}}
+    <ul class="nav nav-tabs">
+    <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">Active</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">Active</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">Active</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">Active</a>
+    </li>
+    </ul>
+    {{--termina navbar--}}
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/portafolio', function () {
+    return view('portafolio');
+})->name('portafolio');
+
+
